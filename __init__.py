3a9a56bb94bbcfcbfbf76267afa09b236c0378ba
Vinayak Trivedi <vinayakt@iitk.ac.in>
2018-10-26 17:25:51 +0530

    project done

---
+++ __init__.py
@@ -0,0 +1,48 @@
+
+from flask import Flask, redirect, url_for, request, make_response,render_template
+import csv
+app = Flask(__name__)
+from flask_mail import Mail, Message
+app.secret_key = 'tSd1zNl8ElzjpAZ0fcQ!7RpejAG3eGD3'
+cities = ["Mumbai","Pune","Bengaluru","Chennai","Kolkata","Hyderabad","NCR","Delhi","Chandigarh","Lucknow","Patna","Varanasi","Guwahati","Ranchi","New Delhi"]
+
+@app.route('/success/<city>')
+def success(city):
+   index = cities.index(city)
+   temp = 0
+   with open('result.csv',"r") as file:
+      rd = csv.reader(file)
+      for item in rd:
+         if(temp == index):
+            return render_template('index.html',data=item)
+         temp += 1
+
+
+@app.route('/setcity')
+def setcity():
+
+   if(request.args.get('city')):
+      city = request.args.get('city')
+      resp = make_response(redirect(url_for('success',city = city)))
+      resp.set_cookie('city', city)
+      return resp
+
+   else:
+      return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+	 
+	if not request.cookies.get('city'):
+		return render_template('login.html')
+	return redirect(url_for('success',city = request.cookies.get('city')))
+
+@app.route('/logout')
+def logout():
+   resp = make_response(redirect(url_for('success',city = city)))
+   resp.set_cookie('city', '', expires=0)
+   return resp
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ tweet_extracter.py
@@ -0,0 +1,136 @@
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+import csv
+import re 
+import requests
+import requests
+from collections import defaultdict
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+def remove_handles(input_txt):
+    pattern = "@[\w]*"
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt)
+        
+    return input_txt
+
+def find_sentiment(input_txt):
+    text_analytics_base_url = "https://centralindia.api.cognitive.microsoft.com/text/analytics/v2.0/"
+    subscription_key = '389726ec04f84154b12f7facf1ae1a05'
+    sentiment_api_url = text_analytics_base_url + "sentiment"
+    documents = {'documents' : [{'id': '1', 'language': 'en', 'text': input_txt}]}
+    headers   = {"Ocp-Apim-Subscription-Key": subscription_key}
+    response  = requests.post(sentiment_api_url, headers=headers, json=documents)
+    sentiments = response.json()
+    return sentiments
+
+def find_phrases(input_txt):
+    text_analytics_base_url = "https://centralindia.api.cognitive.microsoft.com/text/analytics/v2.0/"
+    subscription_key = '389726ec04f84154b12f7facf1ae1a05'
+    phrases_api_url = text_analytics_base_url + "keyPhrases"
+    documents = {'documents' : [{'id': '1', 'language': 'en', 'text': input_txt}]}
+    headers   = {"Ocp-Apim-Subscription-Key": subscription_key}
+    response  = requests.post(phrases_api_url, headers=headers, json=documents)
+    phrases = response.json()
+    return phrases
+
+def find(city):
+    disasters = ["Flood","Tsunami","Earthquake","Rain","Cyclone","Storm","Hurricane","Landslide","Waves","Famine","Violence","Riot"]
+    pos = {}
+    neg = {}
+    for disaster in disasters:
+        pos[disaster] = 0
+        neg[disaster] = 0
+
+    ACCESS_TOKEN = '1051698216943550465-t6GwP9uoXLn6ACR6xNPjaLY2JKFNLA'
+    ACCESS_SECRET = '74vXoNq76SDLl6c4zvO4zRaHHX6SOvBFLrz7RFSTHLi9D'
+    CONSUMER_KEY = 'Jw1rUipyiUZnIaJBTAawMDDFk'
+    CONSUMER_SECRET = 'ZQBJpB84GmRN9mx8RA8T2bVIbHlfCPUfdSWl6T0xXEid9xDQcO'
+
+    oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+    t = TwitterStream(auth=oauth)
+    
+    iterator = t.statuses.filter(track=city, language="en")
+    
+    count = 10
+    rows=[]
+    missed=0
+    for tweet in iterator:
+        if(count<1):
+                break
+        count = count-1
+        text=(tweet['text'])
+        text.replace('(', '')
+        text.replace(')', '')
+        text.replace('"', '')
+        text.replace("'", '')
+        text.replace('\\', '')
+        text = remove_handles(text)
+
+        phrases = find_phrases(text)
+        for item in phrases['documents'][0]['keyPhrases']:
+            for check in disasters:
+                if(check.lower() == item.lower()):
+                    sentiment = find_sentiment(text)
+                    if sentiment['documents'][0]['score'] > 0.45:
+                        pos[check] += 1
+                    else:
+                        neg[check] += 1
+    
+    return pos,neg    
+
+
+def main():
+    new_dic = defaultdict(dict)
+    dec_dic = defaultdict(dict)
+
+    disasters = ["Flood","Tsunami","Earthquake","Rain","Cyclone","Storm","Hurricane","Landslide","Waves","Famine","Violence","Riot"]
+    
+    cities = ["Mumbai","Chennai","Kolkata","Delhi","Lucknow","Ranchi"]
+    for city in cities:
+        for disaster in disasters:
+            new_dic[city][disaster] = 0
+
+    for city in cities:
+        for disaster in disasters:
+            dec_dic[city][disaster] = 0
+
+    while(1):
+        rows = []
+        for city in cities:
+            temp = []
+            temp.append(city)
+            pos,neg = find(city)
+            print(city)
+            for disaster in disasters:
+                if(pos[disaster] > dec_dic[city][disaster]):
+                    temp.append("decreasing")
+                elif(neg[disaster] > new_dic[city][disaster]):
+                    temp.append("increasing")
+                else:
+                    temp.append("Almost constant")
+
+                if(neg[disaster] > 1.8*new_dic[city][disaster]):
+                    temp.append("High Risk")
+                elif(neg[disaster] > 1.4*new_dic[city][disaster]):
+                    temp.append("Moderate Risk")
+
+                else:
+                    temp.append("Low Risk")
+
+                new_dic[city][disaster] = neg[disaster]
+                dec_dic[city][disaster] = pos[disaster]
+
+            rows.append(temp)
+
+        with open('result.csv',"w+") as file:
+            wr=csv.writer(file)
+            wr.writerows(rows)
+
+
+if __name__ == '__main__':
+    main()
